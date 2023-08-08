743bd724601d3d76337d692e181af30971d362c2
Patrick BONNEFOI <bonnefoipatrick@yahoo.fr>
2023-08-08 16:04:49 +0200

    feat : add provider terraform

--- infra/dns.tf
+++ infra/dns.tf
@@ -1,8 +1,18 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "telmate/proxmox"
+      version = ">=2.9.14"
+    }
+  }
+}
+
 provider "proxmox" {
   pm_tls_insecure = true
   pm_api_url = "https://192.168.88.238:8006/api2/json"
   pm_user = "terraform@pam"
   pm_password = "&p1VE28$"
+  pm_debug = true
 }
 
 resource "proxmox_lxc" "lxc-test" {
