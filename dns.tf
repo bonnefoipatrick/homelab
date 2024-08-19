806a765fee31f8f16169e6e5b2d85a7a282fc703
Patrick bonnefoi <bonnefoipatrick@yahoo.fr>
2024-08-19 15:06:46 +0200

    feat : update ip

--- dns.tf
+++ dns.tf
@@ -1,9 +1,10 @@
-resource "proxmox_vm_qemu" "control_plane" {
+resource "proxmox_vm_qemu" "dns" {
   count             = 1
   name              = "lp032dns001"
   target_node       = "${var.pm_node}"
 
   clone             = "ci9000deb001"
+  full_clone        = true
 
   os_type           = "cloud-init"
   cores             = 2
@@ -26,12 +27,18 @@ resource "proxmox_vm_qemu" "control_plane" {
   }
 
   network {
+    id              = 0
+    ip              = "172.0.8.253/24"
+    gateway         = "172.0.8.1"
     model           = "virtio"
     bridge          = "vmbr0"
     tag		    = 8
   }
 
   network {
+    id              = 1
+    ip              = "172.0.32.253/24"
+    gateway         = "172.0.32.1"
     model	    = "virtio"
     bridge	    = "vmbr0"
     tag		    = 32
@@ -39,11 +46,36 @@ resource "proxmox_vm_qemu" "control_plane" {
 
   # cloud-init settings
   # adjust the ip and gateway addresses as needed
-  ipconfig0	    = "ip=172.0.8.253/24,gw=172.0.8.1"
-  ipconfig1         = "ip=172.0.32.253/24,gw=172.0.32.1"
+  #ipconfig0	    = "ip=172.0.8.253/24,gw=172.0.8.1"
+  #ipconfig1         = "ip=172.0.32.253/24,gw=172.0.32.1"
   sshkeys = file("${var.ssh_key_file}")
 
   ciuser            = "uadm001"
   cipassword        = "&p1VE28$"
+
+  # Déclaration du script de démarrage, en utilisant user it-anthony + clé SSH privée
+  provisioner "file" {
+    source      = "~/Documents/Terraform/startup.sh"
+    destination = "/tmp/startup.sh"
+    connection {
+      type     = "ssh"
+      user     = "uadm001"
+      private_key     = "${file("~/.ssh/id_rsa")}"
+      host     = "${self.ssh_host}"
+    }
+  }
+  # Exécution du script de démarrage
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/startup.sh",
+      "/tmp/startup.sh",
+    ]
+    connection {
+      type     = "ssh"
+      user     = "uadm001"
+      private_key     = "${file("~/.ssh/id_rsa")}"
+      host     = "${self.ssh_host}"
+    }
+  }
 }
 
