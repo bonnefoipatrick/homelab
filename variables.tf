011e36e2694dc43eb09cbd1efb989d8a964cdd47
Patrick bonnefoi <bonnefoipatrick@yahoo.fr>
2024-08-19 14:47:52 +0200

    feat : init new feature

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "pm_api_url" {
+default = "https://192.168.88.238:8006/api2/json"
+}
+
+variable "pm_node" {
+default = "pve"
+}
+
+variable "pm_token_id" {
+default = "terraform@pve!terraform-token"
+}
+
+variable "pm_token_secret" {
+default = "612ba55b-e31d-4f80-ae9a-9ae0d92d1667"
+}
+
+variable "ssh_key_file" {
+default = "~/.ssh/key-tools.pub"
+}
